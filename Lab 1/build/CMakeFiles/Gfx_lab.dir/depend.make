62ec55c79e4b0e2c7a437d2013ff348d66321b73
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-03-10 19:43:40 +0200

    Vertex & Fragment Shaders

---
+++ Lab 1/build/CMakeFiles/Gfx_lab.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Gfx_lab.
+# This may be replaced when dependencies are built.
---
+++ Lab 1/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Github/Graphics-Labs/Lab 1
+# Build directory: D:/Github/Graphics-Labs/Lab 1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
